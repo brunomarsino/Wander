d183c4bf2db216d75d317a7da4421bb37700b65f
Bruno Marsino <your-email@example.com>
2026-04-27 16:59:05 -0700

    Use Odyssey client credentials flow

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const SHELL_CACHE = 'wander-shell-v5';
-const RUNTIME_CACHE = 'wander-runtime-v5';
+const SHELL_CACHE = 'wander-shell-v6';
+const RUNTIME_CACHE = 'wander-runtime-v6';
 
 const SHELL_ASSETS = [
     './',
